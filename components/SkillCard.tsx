9eaf7686ee29af73838553b761ed29dd72aae617
Vithushan Namasivayasivam <vithushan19@gmail.com>
2022-10-31 09:53:15 -0400

    Add tailwind and create math home page

---
+++ components/SkillCard.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Link from "next/link";
+
+export default function SkillCard({ skill, courseId }) {
+  const [isSelected, setIsSelected] = useState(false);
+
+  return (
+    <Link href={`/studentPortal/labs/${courseId}/practice/${skill.id}`}>
+      <div
+        onMouseEnter={() => setIsSelected(true)}
+        onMouseLeave={() => setIsSelected(false)}
+        className={`${"cursor-pointer transform transition duration-200 hover:bg-gray-200"} bg-gray-100 w-full mb-4 flex flex-row sm:flex-col items-center h-full rounded-xl shadow-lg`}
+      >
+        <div className="h-full col-span-10">
+          <p className="flex items-center justify-start h-full col-span-8 p-4 text-center text-murkrow">
+            {`I can ${skill.description}`}
+          </p>
+        </div>
+        <div className="h-full col-span-2 rounded-r-lg">
+          <p
+            className={` col-span-2 justify-center rounded-r-lg text-center flex items-center h-full text-4xl mb-4 transform transition-all ease-in-out duration-200 ${
+              isSelected ? "-rotate-45 scale-110" : ""
+            }`}
+          >
+            {getEmoji(99)}{" "}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export const EMOJI_MASTERY = 66;
+export function getEmoji(emojiNum: number | null) {
+  if (emojiNum === undefined) {
+    console.error("Error getting emoji value.");
+    return "⁉️";
+  } else if (emojiNum === null) {
+    return "❓";
+  } else if (emojiNum >= 0 && emojiNum <= 33) {
+    return "😔";
+  } else if (emojiNum >= 34 && emojiNum <= EMOJI_MASTERY) {
+    return "😐";
+  } else {
+    return "😄";
+  }
+}
---
+++ pages/math.tsx
@@ -0,0 +1,45 @@
+import { ApolloClient, InMemoryCache, useQuery } from "@apollo/client";
+
+import CourseNavbar from "../components/CourseNavbar";
+import PracticePreview from "../components/PracticePreview";
+import { FETCH_SKILLS_FOR_COURSE } from "../graphql/fetchSkillsForCourse";
+
+export default function MathHomePage({ courseId, skillData }) {
+  return (
+    <div className="flex flex-col justify-center overflow-auto bg-scroll bg-blue-100 ">
+      <CourseNavbar
+        navbarLinks={[
+          { name: "Practice", href: `/studentPortal/labs/${courseId}` },
+          { name: "Games", href: `/studentPortal/labs/${courseId}/games` },
+        ]}
+      />
+      <div className="flex flex-col p-4 space-y-8">
+        {skillData && (
+          <PracticePreview skills={skillData.skills} courseId={courseId} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps({ params }) {
+  const client = new ApolloClient({
+    uri: "https://talented-duckling-40.hasura.app/v1/graphql/",
+    cache: new InMemoryCache(),
+  });
+
+  const { data } = await client.query({
+    query: FETCH_SKILLS_FOR_COURSE,
+    variables: {
+      courseId: "math1",
+    },
+  });
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return { props: { skillData: data, courseId: "math1" } };
+}
