522941d8915c97ba01c6ee586c8c7bae017fd7c3
LuckyHariharan <lharihar@uwaterloo.ca>
2022-11-10 10:08:47 -0500

    Fix for AlienPathway local host
    Fix for Alien Pathway Local host. The screen wasnt appearing after some change.

--- pages/games/AlienPathway.tsx
+++ pages/games/AlienPathway.tsx
@@ -24,31 +24,29 @@ export default function AlienPathwayV2() {
   };
 
   // Represents Number of Dice Rolls
-  let userProgressHashMap = new Map([[one, 0]]);
+  // let userProgressHashMap = new Map([[one, 0]])
 
-  let firstRow = userProgressHashMap.get(1);
-  console.log(firstRow);
-  if (randomNumber)
-    return (
-      <div className="className='flex justify-center h-screen p-4 space-y-4 bg-slate-800">
-        <Button label={"Roll Dice"} onClick={handleOnClick} />
-        <p className="text-white">this is the dice roll: {randomNumber}</p>
-        test to track user progress
-        <p>{firstRow}</p>
-        <div className="grid grid-cols-7 text-2x`l text-white border-2">
-          {sampleGrid.map((gridData) => (
-            <div className="h-8 border-b-2 border-r-2 md:h-8 sm:h-8">
-              <BlockComponent
-                index={1}
-                rollDisplay={"hello"}
-                currentRoll={1}
-                blockNumber={gridData.id}
-                newGame={0}
-                answer={""}
-              />
-            </div>
-          ))}
-        </div>
+  // let firstRow = userProgressHashMap.get(1);
+  // console.log(firstRow);
+  return (
+    <div className="className='flex justify-center h-screen p-4 space-y-4 bg-slate-800">
+      <Button label={"Roll Dice"} onClick={handleOnClick} />
+      <p className="text-white">this is the dice roll: {randomNumber}</p>
+      test to track user progress
+      <div className="grid grid-cols-7 text-2x`l text-white border-2">
+        {sampleGrid.map((gridData) => (
+          <div className="h-8 border-b-2 border-r-2 md:h-8 sm:h-8">
+            <BlockComponent
+              index={1}
+              rollDisplay={"hello"}
+              currentRoll={1}
+              blockNumber={gridData.id}
+              newGame={0}
+              answer={""}
+            />
+          </div>
+        ))}
       </div>
-    );
+    </div>
+  );
 }
